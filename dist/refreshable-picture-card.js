0d3e5aefa5a6cc0a928f80eb2fe426842e76f224
Dima Goltsman <dimagoltsman@users.noreply.github.com>
2025-03-20 10:24:21 +0200

    Update refreshable-picture-card.js

--- dist/refreshable-picture-card.js
+++ dist/refreshable-picture-card.js
@@ -2,7 +2,7 @@ import {
   LitElement,
   html,
   css,
-} from "https://unpkg.com/lit-element@2.0.1/lit-element.js?module";
+} from "https://unpkg.com/lit-element@4.1.1/lit-element.js?module";
 import "./refreshable-picture-card-editor.js";
 import { handleAction } from "./utils.js";
 
